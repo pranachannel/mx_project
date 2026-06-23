5f5986d744256108f0dc2fd348fae0f159882c04
pranachannel <pranachanneru@gmail.com>
2026-06-23 18:21:47 +0900

    Update dc_crawling.go

--- scripts/dc_crawling.go
+++ scripts/dc_crawling.go
@@ -311,8 +311,8 @@ func scrapePostsAndComments(validPosts []int, collectionTimeStr string, targetSt
 	
 	c.Limit(&colly.LimitRule{
 		DomainGlob:  "*",
-		Parallelism: 3,               
-		Delay:       15 * time.Second, 
+		Parallelism: 2,               
+		Delay:       5 * time.Second, 
 		RandomDelay: 5 * time.Second, 
 	})
 
